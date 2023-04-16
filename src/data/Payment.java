653db424b886e5dead27f74e2cefc5052cae2ae3
MahmoudRabea20 <113620807+MahmoudRabea20@users.noreply.github.com>
2023-04-17 01:49:28 +0200

    Add files via upload

--- src/data/Payment.java
+++ src/data/Payment.java
@@ -1,17 +1,15 @@
-
 package data;
 
 import java.util.Date;
 
-
 public class Payment implements DataEntity {
-    
+
     private String cardType;
     private long cardNumber;
     private String cardHolderName;
     private short cardCVV;
     private Date expirationDate;
-    
+
     public Payment(String cardType, long cardNumber, String cardHolderName, short cardCVV, Date expirationDate) {
         this.cardType = cardType;
         this.cardNumber = cardNumber;
@@ -19,5 +17,45 @@ public class Payment implements DataEntity {
         this.cardCVV = cardCVV;
         this.expirationDate = expirationDate;
     }
-    
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(long cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public short getCardCVV() {
+        return cardCVV;
+    }
+
+    public void setCardCVV(short cardCVV) {
+        this.cardCVV = cardCVV;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
 }
