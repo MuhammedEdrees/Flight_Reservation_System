2410bb8e220ad9b0edbf21a6d10163167d61ac5f
MahmoudRabea20 <113620807+MahmoudRabea20@users.noreply.github.com>
2023-05-03 04:21:17 +0300

    Add files via upload

---
+++ src/model/Payment.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.util.Date;
+
+public class Payment {
+
+    private int paymentId, reservationId;
+    private String cardType, cardHolderName, cardNumber, cardCVV;
+    private double paymentAmount;
+    private Date expirationDate;
+
+    public Payment() {
+    }
+
+    public Payment(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Payment(int reservationId, String cardType, String cardHolderName, String cardNumber, double paymentAmount, String cardCVV, Date expirationDate) {
+        this.reservationId = reservationId;
+        this.cardType = cardType;
+        this.cardHolderName = cardHolderName;
+        this.cardNumber = cardNumber;
+        this.paymentAmount = paymentAmount;
+        this.cardCVV = cardCVV;
+        this.expirationDate = expirationDate;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public String getCardCVV() {
+        return cardCVV;
+    }
+
+    public void setCardCVV(String cardCVV) {
+        this.cardCVV = cardCVV;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+}
