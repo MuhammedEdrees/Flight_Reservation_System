72b856091f8e1c8b3670534a3290575fcf852117
Edrees <muhammed.edrees101@gmail.com>
2023-06-22 23:54:27 +0300

    updated flightagentcontroller class

--- src/controller/FlightAgentController.java
+++ src/controller/FlightAgentController.java
@@ -21,10 +21,20 @@ public class FlightAgentController {
         this.view = view;
     }
 
+    public void setValidator(Validation validator) {
+        this.validator = validator;
+    }
+
+    public void setFlightRepo(FlightRepository flightRepo) {
+        this.flightRepo = flightRepo;
+    }
+
+    public void setReservationRepo(ReservationRepository reservationRepo) {
+        this.reservationRepo = reservationRepo;
+    }
+    
     public void handleAddFlightButtonClick() {
-        if (validator.validateId(view.getFlightId()) == false) {
-            JOptionPane.showMessageDialog(view, "The flight id entered is invalid!", "Invalid Input", JOptionPane.ERROR_MESSAGE);
-        } else if (validator.validateDepartureDate(view.getFlightDate()) == false) {
+        if (validator.validateDepartureDate(view.getFlightDate()) == false) {
             JOptionPane.showMessageDialog(view, "The Date entered is invalid!", "Invalid Input", JOptionPane.ERROR_MESSAGE);
         } else if (validator.validateSeatCapacity(view.getSeatCapacity()) == false) {
             JOptionPane.showMessageDialog(view, "The seat capacity entered is invalid!", "Invalid Input", JOptionPane.ERROR_MESSAGE);
@@ -70,8 +80,8 @@ public class FlightAgentController {
         } else if (validator.validatePayment(view.getBasePrice()) == false) {
             JOptionPane.showMessageDialog(view, "The base price entered is invalid!", "Invalid Input", JOptionPane.ERROR_MESSAGE);
         } else {
-            Flight flight = new Flight(view.getDepartureAirport(), view.getArrivalAirport(), view.getFlightDate(), view.getDepartureTime(), view.getFlightDuration(), view.getBasePrice(), view.getAirline(), view.getSeatCapacity(), view.getSeatCapacity());
-            flightRepo.create(flight);
+            Flight flight = new Flight(view.getFlightId(), view.getDepartureAirport(), view.getArrivalAirport(), view.getFlightDate(), view.getDepartureTime(), view.getFlightDuration(), view.getBasePrice(), view.getAirline(), view.getSeatCapacity(), view.getSeatCapacity());
+            flightRepo.update(flight);
             DefaultTableModel model = view.getFlightTableModel();
             updateFlightsTableModel(model);
 
@@ -85,13 +95,14 @@ public class FlightAgentController {
         } else {
             Flight flight = new Flight(view.getFlightId());
             flightRepo.delete(flight);
+            DefaultTableModel model = view.getFlightTableModel();
+            updateFlightsTableModel(model);
         }
-        DefaultTableModel model = view.getFlightTableModel();
-        updateFlightsTableModel(model);
     }
 
     public void updateFlightsTableModel(DefaultTableModel model) {
         ArrayList<Flight> flightsList = flightRepo.getAll();
+        model.setRowCount(0);
         for (int i = 0; i < flightsList.size(); i++) {
             Object[] flight = {flightsList.get(i).getId(), flightsList.get(i).getDepartureAirport(), flightsList.get(i).getArrivalAirport(), flightsList.get(i).getFlightDate(), flightsList.get(i).getDepartureTime(), flightsList.get(i).getFlightDuration(), flightsList.get(i).getBasePrice()};
             model.addRow(flight);
@@ -100,6 +111,7 @@ public class FlightAgentController {
 
     public void updateReservationsTableModel(DefaultTableModel model) {
         ArrayList<Reservation> reservationList = reservationRepo.getAll();
+        model.setRowCount(0);
         for (int i = 0; i < reservationList.size(); i++) {
             Object[] reservation = {reservationList.get(i).getId(), reservationList.get(i).getFirstName(), reservationList.get(i).getNationality(), reservationList.get(i).getPassNum(), reservationList.get(i).getPassExpiry(), reservationList.get(i).getNumOfseats(), reservationList.get(i).getFlightID(), reservationList.get(i).getPassengerId()};
             model.addRow(reservation);
